c57842f3da2ffa399c8771ba6d93a934baa8ae83
Stoeffel <christoph.hermann@kaba.com>
2015-06-19 08:36:43 +0200

    no es6 :(

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 'use strict';
 var React = require('react');
 
-module.exports = function mini(comp, initialState=null) {
+module.exports = function mini(comp, initialState) {
 	return React.createClass({
 		getInitialState: function() {
-			return initialState;
+			return initialState || null;
 		},
 		render: function() {
 			return comp(this, this.props, this.state, this.props.children);
